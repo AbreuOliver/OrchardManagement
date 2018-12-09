6a0715276577f307885294af06b95190b77f4c78
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-12-08 23:55:31 -0500

    (MVP : SERVER) - imported json-server to for data persistence

--- soulwinners-tool/src/App.js
+++ soulwinners-tool/src/App.js
@@ -1,24 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/header";
+import logo from "./logo.svg";
+import "./db.json";
+import "./App.css";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        soulWinningContacts: []
+      };
+  }
+  // post-mount access (the last step in the birth/mount lifecycle phase)
+  componentDidMount() {
+    let url = "http://localhost:3000/soulWinningContacts"
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        let soulWinningContacts = data.map((soulWinningContacts, index) => {
+          return (
+            <div className="contact-data" key="index">
+              <p>
+                # {soulWinningContacts.id} Name: {soulWinningContacts.firstName} {soulWinningContacts.lastName}
+              </p>
+            </div>
+          )
+        })
+        this.setState({soulWinningContacts: soulWinningContacts});
+      })
+  }
+
   render() {
     return (
       <div className="App">
+        <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            This sample PWA is brought to you by Oliver Abreu.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            Soul Winner's App
+          </h1>
+          <div>
+            {this.state.soulWinningContacts}
+          </div>
         </header>
       </div>
     );
