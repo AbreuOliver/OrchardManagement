7d45d0af75f95058407c34148a42e9dc6261ec0b
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-12-08 23:57:07 -0500

    saved

---
+++ soulwinners-tool/src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Header = props => {
+    return(
+        <div className="header">
+            Orchard Management
+        </div>
+    )
+}
+
+export default Header;
